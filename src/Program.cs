b75c4bd34021bd7b842d4b21c2e9544d8bd644bf
tiagohgarcia <tg.grc1@gmail.com>
2026-09-09 00:13:59 +0200

    Program.cs - read args and start logger

--- src/Program.cs
+++ src/Program.cs
@@ -1,1 +1,46 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using SyncDirs;
+
+internal class Program
+{
+    private static async Task<int> Main(string[] args)
+    {
+        // show help
+        if(args.Contains("-h") || args.Contains("--help"))
+        {
+            Console.WriteLine(CommandLineOptions.HelpText);
+            return 0;
+        }
+
+        // read command line arguments
+        CommandLineOptions options;
+        try
+        {
+            options = CommandLineOptions.Parse(args);
+        }
+        catch (ArgumentException e)
+        {
+            Console.Error.WriteLine($"error: {e.Message}");
+            Console.Error.WriteLine(CommandLineOptions.HelpText);
+            return 1;
+        }
+
+        // start logger
+        Logger log;
+        try
+        {
+            log = new Logger(options.LogPath);
+        }
+        catch(Exception e)
+        {
+            Console.Error.WriteLine($"error: {e.Message}");
+            return 1;
+        }
+
+        using(log)
+        {
+            log.Info(options.ToString());
+
+        }
+        return 0;
+    }
+}
